93a5ded7768fb87b53ca613585827f6725dde439
Christian James Tecson <100465784+Muwon1@users.noreply.github.com>
2024-02-28 17:30:19 +0800

    - Added TaskDetail for DetailPage

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 class TaskList(ListView):
     model = Task 
     template_name = 'todo_app/task_list.html'
     context_object_name = 'tasks'
+    
+class TaskDetail(DetailView):
+    model = Task
