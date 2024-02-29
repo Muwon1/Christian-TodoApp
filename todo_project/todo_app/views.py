181edea26e0bf472afa95165f2e17a68bf438c73
Christian James Tecson <100465784+Muwon1@users.noreply.github.com>
2024-02-29 13:12:10 +0800

    -Added LoginMixin for Authentication

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -4,6 +4,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView,
 from. django.contrib.auth.views import LoginView
 from .models import Task
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class CustomLoginView(LoginView):
@@ -14,26 +15,26 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     model = Task 
     template_name = 'todo_app/task_list.html'
     context_object_name = 'tasks'
     
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class TaskUpdate(UpdateView):    
+class TaskUpdate(LoginRequiredMixin, UpdateView):    
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class DeleteView(DeleteView):
+class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'tasks'
     success_url = reverse_lazy('tasks')
