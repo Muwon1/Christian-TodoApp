f7ace379069776abb05d7e77e8f2bfd5bcc703b2
Christian James Tecson <100465784+Muwon1@users.noreply.github.com>
2024-02-29 16:30:57 +0800

    -Updated TaskList and TaskCreate to get context data and create a valid form

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -19,6 +19,11 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task 
     template_name = 'todo_app/task_list.html'
     context_object_name = 'tasks'
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['object_list'] = context['object_list'].filter(user=self.request.user)
+            context['count'] = context['object_list'].filter(complete=False).count()
+            return context
     
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
@@ -26,12 +31,15 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
+        def form_valid(self, form):
+            form.instance.user = self.request.user
+            return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):    
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 class DeleteView(LoginRequiredMixin, DeleteView):
