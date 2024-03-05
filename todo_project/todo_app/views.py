e473583ca85eb43ddc01e49d8632be4954edaa15
Christian James Tecson <100465784+Muwon1@users.noreply.github.com>
2024-03-05 17:48:28 +0800

    -Added rest_framework imports

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render, Register
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView,, RegisterPage
-from. django.contrib.auth.views import LoginView
-from .models import Task
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-
-
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
 
 
 class CustomLoginView(LoginView):
