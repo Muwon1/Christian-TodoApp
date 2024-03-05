3f9e495492580812eb0b0d7ec6c528753ce7e7e8
Christian James Tecson <100465784+Muwon1@users.noreply.github.com>
2024-03-05 17:41:11 +0800

    -Added api URL

--- todo_project/todo_app/urls.py
+++ todo_project/todo_app/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>', DeleteView.as_view(), name='task-delete'),
+    path('api/', include(router.urls)),
     
     
     
