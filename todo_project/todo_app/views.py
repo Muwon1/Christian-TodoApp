810ad47407c2310cd771a8093072440ec0661adf
Christian James Tecson <100465784+Muwon1@users.noreply.github.com>
2024-03-06 10:08:28 +0800

    -Added Response

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -11,6 +11,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.response import Response
 
 
 class CustomLoginView(LoginView):
