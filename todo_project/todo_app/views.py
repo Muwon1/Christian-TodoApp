ea3211e8a84df35d7f67015ada9f492c1e1235dd
Christian James Tecson <100465784+Muwon1@users.noreply.github.com>
2024-03-11 12:15:08 +0800

    - Fixed blanklines and whitespaces

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+    FormView,
+)
+
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -18,14 +24,33 @@ class CustomLoginView(LoginView):
     template_name = 'todo_app/login.html'
     fields = '__all__'
     redirect_authenticated_user = True
-    
+
     def get_success_url(self):
         return reverse_lazy('tasks')
 
+
+class RegisterPage(FormView):
+    template_name = 'todo_app/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+            return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+
 class TaskList(LoginRequiredMixin, ListView):
-    model = Task 
-    template_name = 'todo_app/task_list.html'
+    model = Task
     context_object_name = 'tasks'
+    template_name = 'todo_app/task_list.html'
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -34,63 +59,49 @@ class TaskList(LoginRequiredMixin, ListView):
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
             queryset = queryset.filter(title__startswith=search_input)
+
         return queryset
-  
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['object_list'] = context['object_list'].filter(user=self.request.user)
-        context['count'] = context['object_list'].filter(complete=False).count()
-        
-    search_input = self.request.GET.get('search-area') or ''
-        if search_input:
-            context['object_list'] = context['object_list'].filter(title__startswith=search_input)
-            context['search_input'] = search_input
-            return context
+        context['count'] = self.get_queryset().filter(complete=False).count()
+        context['search_input'] = self.request.GET.get('search-area', '')
+        return context
 
-class RegisterPage(FormView):
-    template_name ='todo_app/register.html'
-    form_class = UserCreationForm
-    redirect_authenticated_user = True
-    success_url = reverse_lazy('tasks')
-    
-    def form_valid(self, form):
-        user = form.save()
-        if user is not None:
-        login(self.request, user)
-        return super(RegisterPage, self).form_valid(form)
-    
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated:
-        return redirect('tasks')
-        return super(RegisterPage, self).get(*args, **kwargs)
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
+    template_name = 'todo_app/task_detail.html'
+
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
-    
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(TaskCreate, self).form_valid(form)
 
-class TaskUpdate(LoginRequiredMixin, UpdateView):    
+
+def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super(TaskCreate, self).form_valid(form)
+
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
-class DeleteView(LoginRequiredMixin, DeleteView):
+
+class DeleteView(DeleteView):
     model = Task
     context_object_name = 'tasks'
     success_url = reverse_lazy('tasks')
 
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [IsAuthenticated]  
+    permission_classes = [IsAuthenticated]
 
     def list(self, request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -98,5 +109,8 @@ class TaskViewSet(viewsets.ModelViewSet):
             serializer = self.get_serializer(tasks, many=True)
             return Response(serializer.data)
         else:
-            return Response({'detail': 'You need to be logged in to access this endpoint.'}, status=401)
+            return Response({
+                'detail': 'You need to be logged in to access an endpoint'},
+                status=401)
+
 
