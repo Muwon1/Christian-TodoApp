6c14a7d53d96d86aa5331701c2f0e2b4008d87e9
Christian James Tecson <100465784+Muwon1@users.noreply.github.com>
2024-03-12 17:03:10 +0800

    - Added customUser and fixed indentations

--- todo_project/todo_app/serializers.py
+++ todo_project/todo_app/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, CustomUser
 
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-    fields = '__all__'
+        fields = '__all__'
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['age']
