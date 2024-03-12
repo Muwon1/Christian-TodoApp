2c866db8fa953d115a304802181e4923662c634a
Christian James Tecson <100465784+Muwon1@users.noreply.github.com>
2024-03-12 17:08:29 +0800

    -Added site register for CustomUser

--- todo_project/todo_app/admin.py
+++ todo_project/todo_app/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Task
+from .models import CustomUser
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['user', 'age']
+
 
 admin.site.register(Task)
+admin.site.register(CustomUser)
